b8565602c0d334fb3b95adcadef32ddd190ec955
MRRazvi <mrrazvi@icloud.com>
2023-07-12 22:32:40 +0500

    wip

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,6 @@
 <?php
 
+// if file call directory, abort
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
--- wp-project-management.php
+++ wp-project-management.php
@@ -1,5 +1,13 @@
 <?php
 
 /**
- * Plugin Name: WP Project Management
+ * Plugin Name:         WP Project Management
+ * Text Domain:         wppm
  */
+
+// if file call directory, abort
+if (!defined('WPINC')) {
+    die;
+}
+
+define('WP_PROJECT_MANAGEMENT_VERSION', '0.0.1');
